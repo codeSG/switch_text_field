a668bdfc3acad8cbdd2f15ca40775dd3cc860111
codesg <sourabh.8.june.1996@gmail.com>
2022-06-05 19:01:22 +0530

    v0.0.1 along with example, documentation

--- README.md
+++ README.md
@@ -1,39 +1,80 @@
-<!-- 
+
+<!--
 This README describes the package. If you publish this package to pub.dev,
 this README's contents appear on the landing page for your package.
 
 For information about how to write a good package README, see the guide for
-[writing package pages](https://dart.dev/guides/libraries/writing-package-pages). 
+[writing package pages](https://dart.dev/guides/libraries/writing-package-pages).
 
 For general information about developing packages, see the Dart guide for
 [creating packages](https://dart.dev/guides/libraries/create-library-packages)
 and the Flutter guide for
-[developing packages and plugins](https://flutter.dev/developing-packages). 
+[developing packages and plugins](https://flutter.dev/developing-packages).
 -->
 
-TODO: Put a short description of the package here that helps potential users
-know whether this package might be useful for them.
+It is a UI element for a unique text field which can switch to uneditable text widget on icon click and vice versa.
+It is build over general `TextField` and `Text` widget along with icons that helps in switching between two states.
 
 ## Features
-
-TODO: List what your package can do. Maybe include images, gifs, or videos.
+![img.png](img.png)
 
 ## Getting started
 
-TODO: List prerequisites and provide or point to information on how to
-start using the package.
+To use this plugin, add `switch_text_field` as a dependency in your pubspec.yaml file.
 
 ## Usage
 
-TODO: Include short and useful examples for package users. Add longer examples
-to `/example` folder. 
+You can import the package with:
 
 ```dart
-const like = 'sample';
+import 'package:switch_text_field/switch_text_field.dart';
 ```
 
-## Additional information
 
-TODO: Tell users more about the package: where to find more information, how to 
-contribute to the package, how to file issues, what response they can expect 
-from the package authors, and more.
+Use it as follows:
+
+### Material Example 1:
+```dart
+SwitchTextField(),
+```
+Above code will build widget with default parameters, though their are some customizable properties like text_color, icon_color, background_color, icon_background_color, text_icon, text_field_icon etc.
+
+### Material Example 2:
+```dart
+SwitchTextField(  
+  background_color: Colors.black,  
+  icon_color: Colors.black,  
+  icon_background_color: Colors.white,  
+  text_color: Colors.white,  
+  isEditing: false,  
+  text_icon: Icons.cancel_outlined,  
+  text_field_icon: Icons.add,  
+  text_field_hint: "Enter title... ",  
+),
+```
+In the code above, the some customizable properties allows us to change colors, icons and text properties for the widget[Details below].
+
+## Parameters
+
+`background_color` : `[Color]`  Background color for the container.
+
+`icon_color` : `[Color]`Color for the icon.
+
+`icon_background_color` : `[Color]`Background color for icon button.
+
+`text_color` : `[Color]`Color for the text.
+
+`font_size` : `[Double]`Font size for the text and text field.
+
+`text_field_hint` : `[String]`Hint for the text field to be shown at the initial.
+
+`text_field_icon` : `[IconData]`Icon to be shown along text field.
+
+`text_icon` : `[IconData]`Icon to be shown along uneditable text.
+
+`isEditing` : `[Bool]`Current state of the widget: true means TextField is editable otherwise false means non-editable content.
+
+
+
+## How you can help
+[Contribution Guidelines](https://github.com/codeSG/switch_text_field/blob/master/CONTRIBUTING_GUIDE.md)
--- lib/switch_text_field.dart
+++ lib/switch_text_field.dart
@@ -3,73 +3,114 @@ library switch_text_field;
 import 'package:flutter/material.dart';
 
 class SwitchTextField extends StatefulWidget {
-  const SwitchTextField({Key? key}) : super(key: key);
+  // Background color for the container.
+  final Color background_color;
+  // Color for the icon.
+  final Color icon_color;
+  // Background color for icon button.
+  final Color icon_background_color;
+  // Color for the text.
+  final Color text_color;
+
+  // Font size for the text and text field.
+  final double font_size;
+
+  // Hint for the text field to be shown at the initial.
+  final String text_field_hint;
+  // Icon to be shown along text field.
+  final IconData text_field_icon;
+  //Icon to be shown along uneditable text.
+  final IconData text_icon;
+
+  // Current state of the widget: true means TextField is editable otherwise false means non-editable content.
+  final bool isEditing;
+
+  const SwitchTextField(
+      {this.background_color = Colors.blue,
+      this.icon_color = Colors.white,
+      this.icon_background_color = Colors.black,
+      this.text_color = Colors.black,
+      this.font_size = 16.0,
+      this.text_field_hint = "Edit me",
+      this.text_field_icon = Icons.check,
+      this.text_icon = Icons.edit,
+      this.isEditing = true});
 
   @override
   State<SwitchTextField> createState() => _SwitchTextFieldState();
 }
 
 class _SwitchTextFieldState extends State<SwitchTextField> {
-  String content = "";
-  bool open = true;
+  // Edited or shown text.
+  String textContent = "";
   final TextEditingController controller = TextEditingController();
 
+  late bool _isEditing = widget.isEditing;
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Padding(
-        padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
-        child: open
+    return Container(
+        color: widget.background_color,
+        margin: EdgeInsets.all(5),
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        child: _isEditing
             ? Row(
                 children: [
                   Expanded(
                       flex: 8,
                       child: TextField(
+                        showCursor: true,
+                        style: TextStyle(
+                            color: widget.text_color,
+                            fontSize: widget.font_size,
+                            textBaseline: TextBaseline.alphabetic),
+                        cursorColor: widget.text_color,
                         controller: controller,
                         onTap: () {
                           setState(() {
-                            content = controller.text;
-                            print(content);
+                            textContent = controller.text;
                           });
                         },
-                        decoration: InputDecoration(hintText: "Try me"),
+                        decoration:
+                            InputDecoration(hintText: widget.text_field_hint),
                       )),
                   ElevatedButton(
-                    child: Icon(Icons.check, color: Colors.white),
+                    child:
+                        Icon(widget.text_field_icon, color: widget.icon_color),
                     style: ElevatedButton.styleFrom(
                       shape: CircleBorder(),
-                      primary: Colors.blue, // <-- Button color
-                      onPrimary: Colors.red, // <-- Splash color
+                      primary: widget.icon_background_color, // <-- Button color
                     ),
                     onPressed: () {
-                      print("123");
                       setState(() {
-                        content = controller.text;
-                        open = !open;
+                        textContent = controller.text;
+                        _isEditing = !_isEditing;
                       });
-                      print(content);
                     },
                   ),
                 ],
               )
             : Row(
                 children: [
-                  Expanded(flex: 3, child: Text(content)),
+                  Expanded(
+                    flex: 3,
+                    child: Text(
+                      textContent,
+                      style: TextStyle(
+                          color: widget.text_color, fontSize: widget.font_size),
+                    ),
+                  ),
                   ElevatedButton(
-                    child: Icon(Icons.edit, color: Colors.white),
+                    child: Icon(widget.text_icon, color: widget.icon_color),
                     style: ElevatedButton.styleFrom(
                       shape: CircleBorder(),
-                      padding: EdgeInsets.all(0),
-                      primary: Colors.blue, // <-- Button color
-                      // onPrimary: Colors.red, // <-- Splash color
+                      primary: widget.icon_background_color, // <-- Button color
                     ),
                     onPressed: () {
-                      print("123");
                       setState(() {
-                        controller.text = content;
-                        open = !open;
+                        controller.text = textContent;
+                        _isEditing = !_isEditing;
                       });
-                      print(content);
                     },
                   ),
                 ],
