abe4fd0246039f9e788e7e89b5e3fe9d32bac7d4
codesg <sourabh.8.june.1996@gmail.com>
2022-06-05 16:27:47 +0530

    Base code

---
+++ example/android/app/src/main/kotlin/com/example/switch_text_field/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.switch_text_field.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/switch_text_field.dart
+++ lib/switch_text_field.dart
@@ -1,7 +1,78 @@
 library switch_text_field;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class SwitchTextField extends StatefulWidget {
+  const SwitchTextField({Key? key}) : super(key: key);
+
+  @override
+  State<SwitchTextField> createState() => _SwitchTextFieldState();
+}
+
+class _SwitchTextFieldState extends State<SwitchTextField> {
+  String content = "";
+  bool open = true;
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Padding(
+        padding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+        child: open
+            ? Row(
+                children: [
+                  Expanded(
+                      flex: 8,
+                      child: TextField(
+                        controller: controller,
+                        onTap: () {
+                          setState(() {
+                            content = controller.text;
+                            print(content);
+                          });
+                        },
+                        decoration: InputDecoration(hintText: "Try me"),
+                      )),
+                  ElevatedButton(
+                    child: Icon(Icons.check, color: Colors.white),
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      primary: Colors.blue, // <-- Button color
+                      onPrimary: Colors.red, // <-- Splash color
+                    ),
+                    onPressed: () {
+                      print("123");
+                      setState(() {
+                        content = controller.text;
+                        open = !open;
+                      });
+                      print(content);
+                    },
+                  ),
+                ],
+              )
+            : Row(
+                children: [
+                  Expanded(flex: 3, child: Text(content)),
+                  ElevatedButton(
+                    child: Icon(Icons.edit, color: Colors.white),
+                    style: ElevatedButton.styleFrom(
+                      shape: CircleBorder(),
+                      padding: EdgeInsets.all(0),
+                      primary: Colors.blue, // <-- Button color
+                      // onPrimary: Colors.red, // <-- Splash color
+                    ),
+                    onPressed: () {
+                      print("123");
+                      setState(() {
+                        controller.text = content;
+                        open = !open;
+                      });
+                      print(content);
+                    },
+                  ),
+                ],
+              ));
+  }
 }
